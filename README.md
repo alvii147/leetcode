6e770a8a99e516d81b5da541b07ebde2e6fc658f
Zahin Zaman <alvii147@gmail.com>
2021-07-14 16:22:28 -0400

    added difficulty in readme

--- README.md
+++ README.md
@@ -8,26 +8,30 @@ This repository contains solutions to [LeetCode](https://leetcode.com/) problems
 
 <img alt="Algorithms Logo" src=img/algorithms_logo.png width="100"/>
 
-- [x] [Two Sum](Algorithms/1_Two_Sum)
-- [x] [Add Two Numbers](Algorithms/2_Add_Two_Numbers)
+No. | Problem | Difficulty
+--- | --- | ---
+1. | [Two Sum](Algorithms/1_Two_Sum) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+2. | [Add Two Numbers](Algorithms/2_Add_Two_Numbers) | ![Difficulty](https://img.shields.io/badge/Medium-yellow)
 
 ## Database
 
 <img alt="Database Logo" src=img/database_logo.png width="100"/>
 
-- [x] [Combine Two Tables](Database/175_Combine_Two_Tables)
-- [x] [Second Highest Salary](Database/176_Second_Highest_Salary)
-- [x] [Nth Highest Salary](Database/177_Nth_Highest_Salary)
-- [x] [Rank Scores](Database/178_Rank_Scores)
-- [x] [Consecutive Numbers](Database/180_Consecutive_Numbers)
-- [x] [Employees Earning More Than Their Managers](Database/181_Employees_Earning_More_Than_Their_Managers)
-- [x] [Duplicate Emails](Database/182_Duplicate_Emails)
-- [x] [Customers Who Never Order](Database/183_Customers_Who_Never_Order)
-- [x] [Department Highest Salary](Database/184_Department_Highest_Salary)
-- [x] [Department Top Three Salaries](Database/185_Department_Top_Three_Salaries)
-- [x] [Delete Dupicate Emails](Database/196_Delete_Dupicate_Emails)
-- [x] [Rising Temperature](Database/197_Rising_Temperature)
-- [x] [Trips and Users](Database/262_Trips_and_Users)
-- [x] [Big Countries](Database/595_Big_Countries)
-- [x] [Classes More Than 5 Students](Database/596_Classes_More_Than_5_Students)
+No. | Problem | Difficulty
+--- | --- | ---
+175. | [Combine Two Tables](Database/175_Combine_Two_Tables) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+176. | [Second Highest Salary](Database/176_Second_Highest_Salary) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+177. | [Nth Highest Salary](Database/177_Nth_Highest_Salary) | ![Difficulty](https://img.shields.io/badge/Medium-yellow)
+178. | [Rank Scores](Database/178_Rank_Scores) | ![Difficulty](https://img.shields.io/badge/Medium-yellow)
+180. | [Consecutive Numbers](Database/180_Consecutive_Numbers) | ![Difficulty](https://img.shields.io/badge/Medium-yellow)
+181. | [Employees Earning More Than Their Managers](Database/181_Employees_Earning_More_Than_Their_Managers) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+182. | [Duplicate Emails](Database/182_Duplicate_Emails) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+183. | [Customers Who Never Order](Database/183_Customers_Who_Never_Order) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+184. | [Department Highest Salary](Database/184_Department_Highest_Salary) | ![Difficulty](https://img.shields.io/badge/Medium-yellow)
+185. | [Department Top Three Salaries](Database/185_Department_Top_Three_Salaries) | ![Difficulty](https://img.shields.io/badge/Hard-red)
+196. | [Delete Duplicate Emails](Database/196_Delete_Dupicate_Emails) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+197. | [Rising Temperature](Database/197_Rising_Temperature) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+262. | [Trips and Users](Database/262_Trips_and_Users) | ![Difficulty](https://img.shields.io/badge/Hard-red)
+595. | [Big Countries](Database/595_Big_Countries) | ![Difficulty](https://img.shields.io/badge/Easy-green)
+596. | [Classes More Than 5 Students](Database/596_Classes_More_Than_5_Students) | ![Difficulty](https://img.shields.io/badge/Easy-green)
 
--- docs/README_generator.py
+++ docs/README_generator.py
@@ -1,22 +1,84 @@
 import os
 import json
+import requests
 
-with open('categories.json') as categories_file:
-    categories = json.load(categories_file)
+LEETCODE_API_URL = 'https://leetcode.com/api/problems'
+SHIELDS_IO_BADGE_URL = 'https://img.shields.io/badge'
+DIFFICULTY_LUT = {
+    1: {
+        'name': 'Easy',
+        'color': 'green',
+    },
+    2: {
+        'name': 'Medium',
+        'color': 'yellow',
+    },
+    3: {
+        'name': 'Hard',
+        'color': 'red',
+    },
+}
+CATEGORIES_FILE = 'categories.json'
 
-with open('../README.md', 'w') as readme_file:
-    readme_file.write('# LeetCode Problems\n\n')
-    readme_file.write('<img alt="LeetCode Logo" src="img/leetcode_logo.png" width="400"/>\n\n')
-    readme_file.write('This repository contains solutions to [LeetCode](https://leetcode.com/) problems.\n\n')
-    for category in categories:
-        name = category.get('name')
-        logo = category.get('logo')
+def leetcode_api(category):
+    url = f'{LEETCODE_API_URL}/{category.lower()}/'
 
-        readme_file.write(f'## {name}\n\n')
-        readme_file.write(f'<img alt="{name} Logo" src={logo} width="100"/>\n\n')
+    response = requests.get(url)
+    response_json = response.json()
 
-        for problem_dir in os.listdir(f'../{name}/'):
-            problem_name = ' '.join(problem_dir.split('_')[1:])
-            readme_file.write(f'- [x] [{problem_name}]({name}/{problem_dir})\n')
+    data = {}
+    stat_status_pairs = response_json.get('stat_status_pairs')
+    for question in stat_status_pairs:
+        question_id = question.get('stat').get('question_id')
+        question_title = question.get('stat').get('question__title')
+        difficulty = DIFFICULTY_LUT.get(question.get('difficulty').get('level'))
 
-        readme_file.write('\n')
+        data[question_id] = {
+            'title': question_title,
+            'difficulty': difficulty,
+        }
+
+    return data
+
+def categories_data():
+    with open(CATEGORIES_FILE) as categories_file:
+        categories = json.load(categories_file)
+
+    return categories
+
+def difficulty_shields_io(difficulty):
+    name = difficulty.get('name')
+    color = difficulty.get('color')
+    shield = f'{SHIELDS_IO_BADGE_URL}/{name}-{color}'
+
+    return shield
+
+if __name__ == '__main__':
+    with open('../README.md', 'w') as readme_file:
+        readme_file.write('# LeetCode Problems\n\n')
+        readme_file.write('<img alt="LeetCode Logo" src="img/leetcode_logo.png" width="400"/>\n\n')
+        readme_file.write('This repository contains solutions to [LeetCode](https://leetcode.com/) problems.\n\n')
+
+        categories = categories_data()
+        for category in categories:
+            name = category.get('name')
+            logo = category.get('logo')
+            data = leetcode_api(name)
+
+            readme_file.write(f'## {name}\n\n')
+            readme_file.write(f'<img alt="{name} Logo" src={logo} width="100"/>\n\n')
+            readme_file.write('No. | Problem | Difficulty\n')
+            readme_file.write('--- | --- | ---\n')
+
+            for problem_dir in os.listdir(f'../{name}/'):
+                problem_id = problem_dir.split('_')[0]
+
+                problem_data = data.get(int(problem_id))
+                title = problem_data.get('title')
+
+                difficulty = problem_data.get('difficulty')
+                difficulty_shield = difficulty_shields_io(difficulty)
+
+                readme_file.write(f'{problem_id}. | [{title}]({name}/{problem_dir}) | ![Difficulty]({difficulty_shield})\n')
+
+            readme_file.write('\n')
