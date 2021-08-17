555c4afd7e059e54495ee7308b268620ddfa2a87
Zahin Zaman <alvii147@gmail.com>
2021-08-17 17:20:07 -0400

    added progress bar to readme

--- docs/README_generator.py
+++ docs/README_generator.py
@@ -27,18 +27,21 @@ def leetcode_api(category):
     response_json = response.json()
 
     data = {}
+    free_count = 0
     stat_status_pairs = response_json.get('stat_status_pairs')
     for question in stat_status_pairs:
         question_id = question.get('stat').get('frontend_question_id')
         question_title = question.get('stat').get('question__title')
         difficulty = DIFFICULTY_LUT.get(question.get('difficulty').get('level'))
+        if question.get('paid_only') == False:
+            free_count += 1
 
         data[question_id] = {
             'title': question_title,
             'difficulty': difficulty,
         }
 
-    return data
+    return data, free_count
 
 def categories_data():
     with open(CATEGORIES_FILE) as categories_file:
@@ -63,15 +66,20 @@ if __name__ == '__main__':
         for category in categories:
             name = category.get('name')
             logo = category.get('logo')
-            data = leetcode_api(name)
-
-            readme_file.write(f'## {name}\n\n')
-            readme_file.write(f'<img alt="{name} Logo" src={logo} width="100"/>\n\n')
-            readme_file.write('No. | Problem | Difficulty\n')
-            readme_file.write('--- | --- | ---\n')
+            data, free_count = leetcode_api(name)
 
             listdir = os.listdir(f'../{name}/')
             listdir = sorted(listdir, key=lambda x: int(x.split('_')[0]))
+
+            progress_bar = 'https://progress-bar.dev/' + str(round((len(listdir) / free_count) * 100))
+
+            readme_file.write(f'## {name}\n\n')
+            readme_file.write(f'<img alt="{name} Logo" src={logo} width="100"/>\n\n')
+            readme_file.write(f'Completed **{len(listdir)}** out of **{free_count}** (unpaid) problems\n\n')
+            readme_file.write(f'![Progress Bar]({progress_bar})\n\n')
+            readme_file.write('No. | Problem | Difficulty\n')
+            readme_file.write('--- | --- | ---\n')
+
             for problem_dir in listdir:
                 problem_id = problem_dir.split('_')[0]
 
