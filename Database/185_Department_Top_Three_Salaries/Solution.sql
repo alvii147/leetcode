b445a94e381499937a71d214548e9719b354fc76
Zahin <alvii147@gmail.com>
2021-09-26 12:56:19 -0400

    fixed db problem 185

--- Database/185_Department_Top_Three_Salaries/Solution.sql
+++ Database/185_Department_Top_Three_Salaries/Solution.sql
@@ -1,13 +1,17 @@
-Create table If Not Exists Employee (Id int, Name varchar(255), Salary int, DepartmentId int)
-Create table If Not Exists Department (Id int, Name varchar(255))
-Truncate table Employee
-insert into Employee (Id, Name, Salary, DepartmentId) values ('1', 'Joe', '85000', '1')
-insert into Employee (Id, Name, Salary, DepartmentId) values ('2', 'Henry', '80000', '2')
-insert into Employee (Id, Name, Salary, DepartmentId) values ('3', 'Sam', '60000', '2')
-insert into Employee (Id, Name, Salary, DepartmentId) values ('4', 'Max', '90000', '1')
-insert into Employee (Id, Name, Salary, DepartmentId) values ('5', 'Janet', '69000', '1')
-insert into Employee (Id, Name, Salary, DepartmentId) values ('6', 'Randy', '85000', '1')
-insert into Employee (Id, Name, Salary, DepartmentId) values ('7', 'Will', '70000', '1')
-Truncate table Department
-insert into Department (Id, Name) values ('1', 'IT')
-insert into Department (Id, Name) values ('2', 'Sales')
+SELECT Department.Name AS Department, TopEmployees.Name AS Employee, TopEmployees.Salary FROM
+(SELECT Employee.DepartmentId, Employee.Name, Employee.Salary FROM
+(SELECT Salary, DepartmentId FROM
+(SELECT DistinctSalariesA.Salary, DistinctSalariesB.DepartmentId, COUNT(DistinctSalariesB.Salary) AS DepartmentRank FROM
+(SELECT * FROM Employee
+GROUP BY Salary, DepartmentId) AS DistinctSalariesA,
+(SELECT * FROM Employee
+GROUP BY Salary, DepartmentId) AS DistinctSalariesB
+WHERE DistinctSalariesA.Salary <= DistinctSalariesB.Salary AND DistinctSalariesA.DepartmentId = DistinctSalariesB.DepartmentId
+GROUP BY DistinctSalariesA.Id, DistinctSalariesA.Salary) AS RankedSalaries
+WHERE DepartmentRank <= 3) AS TopThreeSalaries
+INNER JOIN Employee
+ON Employee.Salary = TopThreeSalaries.Salary AND Employee.DepartmentId = TopThreeSalaries.DepartmentId)
+AS TopEmployees
+INNER JOIN Department
+ON TopEmployees.DepartmentId = Department.Id
+ORDER BY Department, Employee;
