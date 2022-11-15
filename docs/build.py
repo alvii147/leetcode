36f94c708226f6ce0634ed236bc49677d999c1ed
Zahin Zaman <alvii147@gmail.com>
2022-11-15 10:34:34 -0500

    add languages to build.py

--- docs/build.py
+++ docs/build.py
@@ -18,7 +18,18 @@ DIFFICULTY_LUT = {
         'color': 'orangered',
     },
 }
-CATEGORIES_FILE = 'categories.json'
+CATEGORY_LOGOS = {
+    'Algorithms': 'docs/img/algorithms_logo.png',
+    'Database': 'docs/img/database_logo.png',
+    'Shell': 'docs/img/shell_logo.png',
+    'Concurrency': 'docs/img/concurrency_logo.png',
+}
+LANGUAGE_LOGOS = {
+    'Bash': 'bash-4EAA25?style=for-the-badge&logo=gnubash&logoColor=000000',
+    'Go': 'go-00ADD8?style=for-the-badge&logo=go&logoColor=FFFFFF',
+    'MySQL': 'mysql-4479A1?style=for-the-badge&logo=mysql&logoColor=F29111',
+    'Python': 'python-3670A0?style=for-the-badge&logo=python&logoColor=FFDD54',
+}
 
 def leetcode_api(category):
     url = f'{LEETCODE_API_URL}/{category.lower()}/'
@@ -43,12 +54,6 @@ def leetcode_api(category):
 
     return data, free_count
 
-def categories_data():
-    with open(CATEGORIES_FILE) as categories_file:
-        categories = json.load(categories_file)
-
-    return categories
-
 def difficulty_shields_io(difficulty):
     name = difficulty.get('name')
     color = difficulty.get('color')
@@ -62,23 +67,20 @@ if __name__ == '__main__':
         readme_file.write('<img alt="LeetCode Logo" src="docs/img/leetcode_logo.png" width="400"/>\n\n')
         readme_file.write('This repository contains solutions to [LeetCode](https://leetcode.com/) problems.\n\n')
 
-        categories = categories_data()
-        for category in categories:
-            name = category.get('name')
-            logo = category.get('logo')
-            data, free_count = leetcode_api(name)
+        for category_name, category_logo in CATEGORY_LOGOS.items():
+            data, free_count = leetcode_api(category_name)
 
-            listdir = os.listdir(f'../{name}/')
+            listdir = os.listdir(f'../{category_name}/')
             listdir = sorted(listdir, key=lambda x: int(x.split('_')[0]))
 
             progress_bar = 'https://progress-bar.dev/' + str(round((len(listdir) / free_count) * 100))
 
-            readme_file.write(f'## {name}\n\n')
-            readme_file.write(f'<img alt="{name} Logo" src={logo} width="100"/>\n\n')
+            readme_file.write(f'## {category_name}\n\n')
+            readme_file.write(f'<img alt="{category_name} Logo" src={category_logo} width="100"/>\n\n')
             readme_file.write(f'Completed **{len(listdir)}** out of **{free_count}** (unpaid) problems\n\n')
             readme_file.write(f'![Progress Bar]({progress_bar})\n\n')
-            readme_file.write('No. | Problem | Difficulty\n')
-            readme_file.write('--- | --- | ---\n')
+            readme_file.write('No. | Problem | Difficulty | Languages\n')
+            readme_file.write('--- | --- | --- | ---\n')
 
             for problem_dir in listdir:
                 problem_id = problem_dir.split('_')[0]
@@ -89,6 +91,8 @@ if __name__ == '__main__':
                 difficulty = problem_data.get('difficulty')
                 difficulty_shield = difficulty_shields_io(difficulty)
 
-                readme_file.write(f'{problem_id} | [{title}]({name}/{problem_dir}) | ![Difficulty]({difficulty_shield})\n')
+                language_badges = ' '.join([f'![]({SHIELDS_IO_BADGE_URL}/{LANGUAGE_LOGOS[language]})' for language in os.listdir(f'../{category_name}/{problem_dir}') if language in LANGUAGE_LOGOS])
+
+                readme_file.write(f'{problem_id} | [{title}]({category_name}/{problem_dir}) | ![Difficulty]({difficulty_shield}) | {language_badges}\n')
 
             readme_file.write('\n')
