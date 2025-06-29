7c8fe012e2456f0c20cc4f619f3f99d69a2cd20a
alvii147 <alvii147@gmail.com>
2025-06-29 12:21:01 -0400

    update solution for merge k sorted lists problem

--- Algorithms/23_Merge_k_Sorted_Lists/Python/Solution.py
+++ Algorithms/23_Merge_k_Sorted_Lists/Python/Solution.py
@@ -1,5 +1,36 @@
 import heapq
 
+class ComparableListNode:
+    '''
+    Wrapper around ListNode that implements comparison methods.
+    '''
+    def __init__(self, node: ListNode):
+        self.node = node
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        return f'{type(self).__name__}({self.node.val})'
+
+    def __eq__(self):
+        return self.node.val == other.node.val
+
+    def __eq__(self):
+        return self.node.val != other.node.val
+
+    def __lt__(self, other: ListNode) -> bool:
+        return self.node.val < other.node.val
+
+    def __gt__(self, other: ListNode) -> bool:
+        return self.node.val > other.node.val
+
+    def __le__(self, other: ListNode) -> bool:
+        return self.node.val <= other.node.val
+
+    def __ge__(self, other: ListNode) -> bool:
+        return self.node.val >= other.node.val
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,48 +38,34 @@ import heapq
 #         self.next = next
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        myheap = []
-        # add values from head node to list
-        for node in lists:
-            if node is not None:
-                myheap.append(node.val)
+        # initialize heap with ListNode instances wrapped in ComparableListNode
+        heap = []
+        for headNode in lists:
+            if headNode is None:
+                continue
 
-        # heapify created list
-        heapq.heapify(myheap)
+            heap.append(ComparableListNode(node=headNode))
 
-        # for getting return node first time
-        first = True
-        # previous node
-        prevNode = None
-        # head of node of sorted linked list
-        sortedNode = None
-        # loop till heap is empty
-        while len(myheap) > 0:
-            # pop min value
-            minVal = heapq.heappop(myheap)
-            # create new node with minimum value
-            currentNode = ListNode(val=minVal)
-            if first:
-                # get return node first time
-                sortedNode = currentNode
-                first = False
+        heapq.heapify(heap)
 
-            if prevNode is not None:
-                # make previous node point to current node
-                prevNode.next = currentNode
-            # set current node as previous
-            prevNode = currentNode
+        # create a dummy node to avoid handling edge case of assigning a head node on first iteration
+        dummyNode = ListNode()
+        # pointer of tail node of sorted list
+        tail = dummyNode
 
-            # iterate over head nodes of sorted lists
-            for nodeIdx in range(len(lists)):
-                if lists[nodeIdx] is not None and lists[nodeIdx].val == minVal:
-                    # if min value is found, make head point to next element in sorted list
-                    lists[nodeIdx] = lists[nodeIdx].next
+        # keep merging into sorted list until heap is empty
+        while len(heap) > 0:
+            # pop node with lowest value from heap
+            minCmpNode = heapq.heappop(heap)
+            # make tail of sorted list point to node with lowest value
+            tail.next = minCmpNode.node
+            # advance tail of sorted list to node with lowest value
+            tail = tail.next
 
-                    if lists[nodeIdx] is not None:
-                        # push new value of node to heap
-                        heapq.heappush(myheap, lists[nodeIdx].val)
+            if minCmpNode.node.next is not None:
+                # if node with lowest value is not terminal node
+                # then advance it and push it to heap
+                minCmpNode.node = minCmpNode.node.next
+                heapq.heappush(heap, minCmpNode)
 
-                    break
-
-        return sortedNode
+        return dummyNode.next
